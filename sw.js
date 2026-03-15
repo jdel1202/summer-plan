0e793f6bb683f6e86aa3877d07fe57e520a97b8f
JD <jackdelaney@Jacks-MacBook-Air.local>
2026-03-15 12:16:23 +0000

    Initial commit

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE = 'summer-plan-v2';
+
+const ASSETS = [
+  './App.html',
+  './icon.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.umd.js',
+  'https://fonts.googleapis.com/css2?family=Barlow:wght@400;500;600&family=Barlow+Condensed:wght@600;700&display=swap',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(() => {})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).catch(() => {}))
+  );
+});
